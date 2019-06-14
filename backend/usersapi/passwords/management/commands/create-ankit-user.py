7babfd7c0638314a253f422f76d3206d9d72c14d
ankit <ankit.j.arora@gmail.com>
2019-06-14 11:36:42 +0530

    updated command

--- backend/usersapi/passwords/management/commands/create-ankit-user.py
+++ backend/usersapi/passwords/management/commands/create-ankit-user.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create_user('ankit', password='Password@1234')
-        user.is_superuser = False
-        user.is_staff = False
-        user.save()
+        try:
+            user = User.objects.create_user('ankit', password='Password@1234')
+            user.is_superuser = False
+            user.is_staff = False
+            user.save()
+        except:
+            print("test user already exists")
+            return
         print("created test user")
